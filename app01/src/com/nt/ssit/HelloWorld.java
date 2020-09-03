a565255d3a50d941feeb4fcfafba547851fcf285
Siva <Siva@507>
2020-09-03 08:24:32 +0530

    by siva

--- app01/src/com/nt/ssit/HelloWorld.java
+++ app01/src/com/nt/ssit/HelloWorld.java
@@ -2,6 +2,12 @@ package com.nt.ssit;
 
 public class HelloWorld {
 	private String ename;
+	private String eno;
+	public void setEno(String eno) {
+		this.eno = eno;
+	}public String getEno() {
+		return eno;
+	}
 	public void setEname(String ename) {
 		this.ename = ename;
 	}
