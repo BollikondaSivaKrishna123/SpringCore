b30525703ed2517685cd641d39f3b4461d5909ee
Siva <Siva@507>
2020-09-03 08:45:08 +0530

    17/507 Removed Some methods

--- app01/src/com/nt/ssit/HelloWorld.java
+++ app01/src/com/nt/ssit/HelloWorld.java
@@ -22,14 +22,7 @@ public class HelloWorld {
 		System.out.println("Bollikonda Siva Krishna");
 		return " Hello Guru , "+ename;
 	}
-	public String sayBye()
-	{
-		return "Bye Friends ";
-	}
-public String saySSIT()
-	{
-		System.out.println("Sai Spurthi Students ");
-		return "We are all Ssit Students ";
-	}
+	
+
 
 }
